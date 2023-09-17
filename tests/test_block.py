fbbb63f5b2bc3e3bde451a2506d19bb7bf067904
m-r-g-t <m-r-g-t@defi.sucks>
2023-09-17 20:53:54 +0100

    refactor: block pytest comments

--- tests/test_block.py
+++ tests/test_block.py
@@ -6,6 +6,7 @@ from mev_inspect.block import _find_or_fetch_miner_address
 from tests.utils import load_test_block
 
 
+# Set up generic mock for web3
 @pytest.fixture
 def mocked_web3():
     with patch("mev_inspect.block.Web3") as mock_web3:
@@ -15,51 +16,48 @@ def mocked_web3():
 @pytest.mark.asyncio
 # pylint: disable=redefined-outer-name
 async def test_eth1_block_miner(mocked_web3):
-    # Create a mock Web3 instance
-    mock_web3_instance = mocked_web3.return_value
 
-    # Set up the mock for web3.eth.get_block
+    # instantiate the mock
+    mock_web3_instance = mocked_web3.return_value
     mock_eth = mock_web3_instance.eth
     mock_eth.get_block.return_value = {
         "miner": "0x4a536c1f6a5d5a9c1aeca9f6d04fbbf5f0d8f4e3"
     }
 
-    # Load a sample block and remove the miner
+    # load a sample block and remove the miner
     block_number = 10921991
     block = load_test_block(block_number)
     block.miner = None
 
-    # Test that the miner is fetched
+    # test that the miner is fetched
     miner_address = await _find_or_fetch_miner_address(
         w3=mock_web3_instance, traces=block.traces, block_number=block_number
     )  # Use 'await'
 
-    # this is within the traces object
+    # test it is fetched from traces object
     assert miner_address == "0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5"
 
 
 @pytest.mark.asyncio
 # pylint: disable=redefined-outer-name
 async def test_eth2_block_miner(mocked_web3):
-    # Create a mock Web3 instance
+    # instantiate the mock
     mock_web3_instance = mocked_web3.return_value
-
-    # Create a coroutine function to mock w3.eth.get_block
     # pylint: disable=unused-argument
     async def mock_get_block(block_number):
         return {"miner": "0x4a536c1f6a5d5a9c1aeca9f6d04fbbf5f0d8f4e3"}
 
-    # Mock w3.eth.get_block with the coroutine function
     mock_web3_instance.eth.get_block = MagicMock(side_effect=mock_get_block)
 
-    # Load a sample block and remove the miner
+    # load a sample block and remove the miner
     block_number = 10921990
     block = load_test_block(block_number)
     block.miner = None
 
-    # Test that the miner is fetched
+    # test that the miner is fetched
     miner_address = await _find_or_fetch_miner_address(
         w3=mock_web3_instance, traces=block.traces, block_number=block_number
-    )  # Use 'await'
+    )
 
+    # test miner is fectched from mocked object
     assert miner_address == "0x4a536c1f6a5d5a9c1aeca9f6d04fbbf5f0d8f4e3"
